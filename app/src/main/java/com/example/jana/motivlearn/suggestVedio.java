e8a67210ddfe4a906c2ca33464fadc1440f673cc
noufalsedairy <nouf-s@windowslive.com>
2018-02-08 14:23:55 +0300

    Suggest video - fillblank - codeoutput - multible choices String file not completed yet --> couse some errors

---
+++ app/src/main/java/com/example/jana/motivlearn/suggestVedio.java
@@ -0,0 +1,13 @@
+package com.example.jana.motivlearn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class suggestVedio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suggest_vedio);
+    }
+}
