e042cd6e834e1a21872b553860a1b0922521823a
haifamajid <haifamajid96@gmail.com>
2018-02-20 10:38:31 +0300

    4th push =)

--- app/src/main/java/com/example/jana/motivlearn/RecordAchievement.java
+++ app/src/main/java/com/example/jana/motivlearn/RecordAchievement.java
@@ -1,20 +1,30 @@
 package com.example.jana.motivlearn;
 
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.bestsoft32.tt_fancy_gif_dialog_lib.TTFancyGifDialog;
+import com.bestsoft32.tt_fancy_gif_dialog_lib.TTFancyGifDialogListener;
 import com.budiyev.android.codescanner.AutoFocusMode;
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
 import com.budiyev.android.codescanner.DecodeCallback;
 import com.budiyev.android.codescanner.ErrorCallback;
+import com.example.jana.motivlearn.model.recordAchievementImp;
+import com.example.jana.motivlearn.presenter.recordAchievementPresenter;
+import com.example.jana.motivlearn.view.recordAchievementView;
 import com.google.zxing.Result;
 
-public class RecordAchievement extends AppCompatActivity {
+import java.util.Scanner;
+
+public class RecordAchievement extends AppCompatActivity implements recordAchievementView {
     private CodeScanner mCodeScanner;
+    private String res;
+    private recordAchievementPresenter pres;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +45,45 @@ public class RecordAchievement extends AppCompatActivity {
                             public void run() {
                                 Toast.makeText(RecordAchievement.this, result.getText(),
                                         Toast.LENGTH_LONG).show();
+                                res = result.getText().trim();
+                                if(res.equals("MotivLearn"))
+                                {
+                                    SharedPreferences sp1= getSharedPreferences("Login", MODE_PRIVATE);
+                                    int uid =sp1.getInt("user_id", 0);
+                                    pres= new recordAchievementImp(RecordAchievement.this);
+                                 //   pres.updateUserCoins(uid);
+                                    new TTFancyGifDialog.Builder(RecordAchievement.this)
+                                            .setTitle("Scanned successfully")
+                                            .setMessage("Good job, \n your achievement have been recorded \n and you have got 10 Coins")
+                                            .setPositiveBtnText("Ok")
+                                            .setPositiveBtnBackground("#9577bc")
+                                            .setGifResource(R.drawable.hgif3)      //pass your gif, png or jpg
+                                            .isCancellable(true)
+                                            .OnPositiveClicked(new TTFancyGifDialogListener() {
+                                                @Override
+                                                public void OnClick() {
+                                                    //Toast.makeText(WatchVideo.this,"Ok",Toast.LENGTH_SHORT).show();
+                                                    finish();
+                                                }
+                                            })
+                                            .build();
+                                }
+                                else
+                                {new TTFancyGifDialog.Builder(RecordAchievement.this)
+                                        .setTitle("Incorrect QR code")
+                                        .setMessage("Check the correct QR code, then try again")
+                                        .setPositiveBtnText("Ok")
+                                        .setPositiveBtnBackground("#9577bc")
+                                        .setGifResource(R.drawable.hgif4)      //pass your gif, png or jpg
+                                        .isCancellable(true)
+                                        .OnPositiveClicked(new TTFancyGifDialogListener() {
+                                            @Override
+                                            public void OnClick() {
+                                                //Toast.makeText(WatchVideo.this,"Ok",Toast.LENGTH_SHORT).show();
+                                                finish();
+                                            }
+                                        })
+                                        .build();}
                             }
                         });
                     }
